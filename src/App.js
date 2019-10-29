eea6704bc9be3fb44a02b26ed8a98bafb082e97a
AshuSrv <ashutoshmishra422@gmail.com>
2019-10-29 12:27:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Facebook from './components/Facebook'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    username1:null,
+    user: {
+    username: null,
+    accessToken: null,
+    data_access_expiration_time: null,
+    expiresIn: null,
+    signedRequest: null,
+    userID: null
+    }
+  };
+
+  onFacebookLogin = (loginStatus, resultObject) => {
+    if (loginStatus === true) {
+      console.log(resultObject)
+      console.log(loginStatus)
+      this.setState({
+        username1: resultObject.user.name,
+        user: {
+        username: resultObject.user.name,
+        accessToken: resultObject.authResponse.accessToken,
+        data_access_expiration_time: resultObject.authResponse.data_access_expiration_time,
+        expiresIn: resultObject.authResponse.expiresIn,
+        signedRequest: resultObject.authResponse.signedRequest,
+        uid: resultObject.authResponse.userID,
+        }
+      });
+    } else {
+      alert('Facebook login error');
+    }
+  }
+  componentDidUpdate() {
+    axios.post('http://localhost:3001/create', this.state.user)
+      .then(response => {
+        console.log('auth response', response)
+      }).catch(error => {
+        console.log('auth error', error);
+      });
+  }
+
+
+  render() {
+    const { username1 } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Social Media Login</h1>
+        </header>
+
+        <div className="App-intro">
+          { !username1 &&
+            <div>
+              <p>Click on one of any button below to login</p>
+              <Facebook onLogin={this.onFacebookLogin}>
+                <button>Facebook</button>
+              </Facebook>
+            </div>
+          }
+          {username1 &&
+            <p>Welcome back, {username1}</p>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
